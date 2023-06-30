a79e7cfe3877570063bd51708555e5b7ca93df48
Junhyuk Park <junhyeg08@gmail.com>
2023-06-30 23:42:13 +0900

    chore: rename to randombox

--- contracts/randombox/CreateRandomBox.sol
+++ contracts/randombox/CreateRandomBox.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.10;
 
 import './Token.sol';
-import { BoxWizard } from "./BoxWizard.sol";
+import { RandomBox } from "./RandomBox.sol";
 
 contract CreateRandomBox {
     // Token token;
 
-    struct BoxWizardInfo {
+    struct RandomBoxInfo {
         address msgSender;
-        BoxWizard boxWizardAddress;
+        RandomBox randomBoxAddress;
     }
 
-    BoxWizardInfo[] public boxWizardInfoArray;
+    RandomBoxInfo[] public randomBoxInfoArray;
 
     function getRandomBox() public {
         uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, msg.sender)));
@@ -21,39 +21,39 @@ contract CreateRandomBox {
         uint16 boxGrade;
         address owner = msg.sender;
 
-        if (index <= 9) {                // 0.0009%
+        if (index <= 9) {                // 1 -> 0.0001%
             boxGrade = 1;
-        } else if (index <= 1009) {      // 0.1%
+        } else if (index <= 1000) {
             boxGrade = 2;
-        } else if (index <= 21009) {     // 2%
+        } else if (index <= 20000) {
             boxGrade = 3;
-        } else if (index <= 121009) {    // 10%
+        } else if (index <= 100000) {
             boxGrade = 4;
-        } else if (index <= 421009) {    // 30%
+        } else if (index <= 500000) {
             boxGrade = 5;
-        } else {                         // 55%
+        } else {
             boxGrade = 6;
         }
 
         require(boxGrade != 0, "This is the wrong box!");
-        BoxWizard newBoxWizard = new BoxWizard(boxGrade, owner);
-        boxWizardInfoArray.push(BoxWizardInfo(owner, newBoxWizard));
+        RandomBox newRandomBox = new RandomBox(boxGrade, owner);
+        randomBoxInfoArray.push(RandomBoxInfo(owner, newRandomBox));
     }
 
-    function getMyRandomBox(address _myAddress) view public returns(BoxWizard[] memory) {
+    function getMyRandomBox(address _myAddress) view public returns(RandomBox[] memory) {
         uint256 j = 0;
         uint256 k = 0;
-        
-        for (uint256 i = 0; i < boxWizardInfoArray.length; i++){
-            if(boxWizardInfoArray[i].msgSender == _myAddress){
+
+        for (uint256 i = 0; i < randomBoxInfoArray.length; i++){
+            if(randomBoxInfoArray[i].msgSender == _myAddress){
                 j++;
             }
         }
 
-        BoxWizard[] memory myRandomBox = new BoxWizard[](j);
-        for (uint256 i = 0; i < boxWizardInfoArray.length; i++){
-            if(boxWizardInfoArray[i].msgSender == _myAddress){
-                myRandomBox[k] = boxWizardInfoArray[i].boxWizardAddress;
+        RandomBox[] memory myRandomBox = new RandomBox[](j);
+        for (uint256 i = 0; i < randomBoxInfoArray.length; i++){
+            if(randomBoxInfoArray[i].msgSender == _myAddress){
+                myRandomBox[k] = randomBoxInfoArray[i].randomBoxAddress;
                 k++;
             }
         }
